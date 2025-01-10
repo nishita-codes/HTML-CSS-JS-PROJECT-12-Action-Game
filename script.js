f454265c648625edae787dc8b112e51e950efc87
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-10 23:55:55 +0530

    added files

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Welcome to Action Game");
+document.onkeydown = function(e){
+    console.log("key code is:", e.keyCode)
+    if(e.keyCode == 38){
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+}
