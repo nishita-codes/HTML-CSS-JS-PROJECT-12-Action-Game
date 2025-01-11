4a8cba9800d9c816fb104cec14da80207d39fdc4
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-11 23:56:10 +0530

    added files

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Welcome to Action Game");
+
 document.onkeydown = function(e){
     console.log("key code is:", e.keyCode)
     if(e.keyCode == 38){
@@ -8,4 +9,34 @@ document.onkeydown = function(e){
             dino.classList.remove('animateDino')
         }, 700);
     }
+    if(e.keyCode == 39){
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
 }
+if(e.keyCode == 37){
+    dino = document.querySelector('.dino');
+    dinoX = parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
+    dino.style.left = dinoX - 112 + "px";
+}
+};
+
+setInterval(() =>{
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino , null).getPropertyValue('top'));
+
+    ox = window.getComputedStyle(obstacle  , null).getPropertyValue('left');
+    oy = window.getComputedStyle(obstacle  , null).getPropertyValue('top');
+
+    offsetX = Math.abs(dx-ox);
+    offsetY = Math.abs(dy-oy);
+    if(offsetX < 73 && offsetY < 52){
+        gameOver.style.visibility = "visible";
+        obstacle.classList.remove('obstacleAni');
+    }
+    
+}, 10);
