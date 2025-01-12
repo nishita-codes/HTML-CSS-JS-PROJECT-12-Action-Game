83b21f5def9609cc21d3b4ef36c8bb9637f52130
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-12 22:00:06 +0530

    added files

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("Welcome to Action Game");
 
+ audio = new Audio('music.mp3');
+ audiogo = new Audio('gameover.mp3');
+setTimeout(() => {
+    audio.play();
+},1000);
 document.onkeydown = function(e){
     console.log("key code is:", e.keyCode)
     if(e.keyCode == 38){
@@ -29,14 +34,20 @@ setInterval(() =>{
     dx = parseInt(window.getComputedStyle(dino , null).getPropertyValue('left'));
     dy = parseInt(window.getComputedStyle(dino , null).getPropertyValue('top'));
 
-    ox = window.getComputedStyle(obstacle  , null).getPropertyValue('left');
-    oy = window.getComputedStyle(obstacle  , null).getPropertyValue('top');
+    ox =parseInt(window.getComputedStyle(obstacle  , null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle  , null).getPropertyValue('top'));
 
     offsetX = Math.abs(dx-ox);
     offsetY = Math.abs(dy-oy);
+    console.log(offsetX ,offsetY);
     if(offsetX < 73 && offsetY < 52){
-        gameOver.style.visibility = "visible";
+        gameOver.innerHTML ="Game Over -- Refresh To Restart";
         obstacle.classList.remove('obstacleAni');
+        audiogo.play();
+        setTimeout(() =>{
+            audiogo.pause();
+            audio.pause();
+        },1000);
     }
     
 }, 10);
